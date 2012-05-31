f309ddab53c27ed78b8e730f5d73921a2e3bb3bb
Antonio Grass <asgrass@uc.cl>
2012-05-31 11:09:53 -0400

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitlag::Application.config.secret_token = '30dd889e6efd574b83c9f6ba81615dcd818e7ddb0e6e651355882ee0154817a0009034c2046e57b7cf72bd2bf70cede1cfc817dad84e43cb119229a30272ed5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bitlag::Application.config.session_store :cookie_store, key: '_bitlag_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bitlag::Application.config.session_store :active_record_store
