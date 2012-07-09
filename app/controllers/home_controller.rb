8915a3e7540235e5106470658404f219f35e3367
Antonio Grass <asgrass@uc.cl>
2012-07-09 19:30:40 -0400

    cambio uri fb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   
   
    def index  
+       session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/callback')
+    @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"email, user_events, friends_events")  
+    puts session.to_s + "<<< session"
+     
     respond_to do |format|
        format.html {  }
      end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 APP_ID= '226439280795021' # please change!
 APP_SECRET= '2c8b79cab5d6bae38e6e5afe1758e0e2' # please change!
-SITE_URL = 'http://http://iwuanago.herokuapp.com'
+SITE_URL = 'http://iwuanago.herokuapp.com'
