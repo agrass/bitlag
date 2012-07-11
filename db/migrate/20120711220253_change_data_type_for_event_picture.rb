005ec603e1a6c6c6dff14c74ed36705c6f13b9b1
Antonio Grass <asgrass@uc.cl>
2012-07-11 18:35:40 -0400

    denuevo cambio schema description

--- db/migrate/20120711220253_change_data_type_for_event_picture.rb
+++ db/migrate/20120711220253_change_data_type_for_event_picture.rb
@@ -2,14 +2,13 @@ class ChangeDataTypeForEventPicture < ActiveRecord::Migration
   def up
      change_table :events do |t|
       t.change :picture, :text
-      t.change :description, :text
+      
     end
   end
 
   def down
      change_table :events do |t|
-      t.change :picture, :string
-      t.change :description, :string
+      t.change :picture, :string      
     end
   end
 end
---
+++ db/migrate/20120711223418_change_data_type_for_event_description.rb
@@ -0,0 +1,13 @@
+class ChangeDataTypeForEventDescription < ActiveRecord::Migration
+  def up
+      change_table :events do |t|      
+      t.change :description, :text
+    end
+  end
+
+  def down
+       change_table :events do |t|      
+      t.change :description, :string
+    end
+  end
+end
