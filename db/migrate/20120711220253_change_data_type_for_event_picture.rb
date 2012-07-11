352e496688a734aa03c7793cd1e004e7b0cc4d76
Antonio Grass <asgrass@uc.cl>
2012-07-11 18:04:05 -0400

    cambio migrate

---
+++ db/migrate/20120711220253_change_data_type_for_event_picture.rb
@@ -0,0 +1,13 @@
+class ChangeDataTypeForEventPicture < ActiveRecord::Migration
+  def up
+     change_table :events do |t|
+      t.change :picture, :text
+    end
+  end
+
+  def down
+     change_table :events do |t|
+      t.change :picture, :string
+    end
+  end
+end
