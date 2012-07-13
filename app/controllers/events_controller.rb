8eeb38c00755b68b3ac8ad6a75ab22d4de298dad
Antonio Grass <asgrass@uc.cl>
2012-07-13 18:16:03 -0400

    agrege mapa

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
+     @json = @event.to_gmaps4rails
 
     respond_to do |format|
       format.html # show.html.erb
