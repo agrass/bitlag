fef218416970a49c61238b278e8b6c41ceff62e8
Antonio Grass <asgrass@uc.cl>
2012-08-08 13:36:12 -0400

    cambio tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ Expression.create(:expression => '(\s[Ff]|^[Ff])[Oo0][Nn][Dd][Aa@] ', :tag_id =>
 
 #Filtros para eventos Culturales
 
-Expression.create(:expression => '(\s[Ee]|^[Ee])[Xx][Pp][Oo0][SsCc][Ii][OoóÓ][Nn] ', :tag_id => Tag.find_by_name('exposicion').id )
+Expression.create(:expression => '(\s[Ee]|^[Ee])[Xx][Pp][Oo0][SsCc][Ii][Ooó][Nn] ', :tag_id => Tag.find_by_name('exposicion').id )
 Expression.create(:expression => '(\s[Cc]|^[Cc])[Hh][Aa][Rr][Ll][Aa@] ', :tag_id => Tag.find_by_name('charla').id )
 Expression.create(:expression => '(\s[CckK]|^[CckK])[Aa][Ff][EeéÉ][Cc][Oo0][Nn][Cc][Ee3][Rr][Tt] ', :tag_id => Tag.find_by_name('cafeconcert').id )
 Expression.create(:expression => '(\s[Oo0]|^[Oo0])[Bb][Rr][Aa] ', :tag_id => Tag.find_by_name('obra').id )
