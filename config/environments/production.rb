d584f7bfd5c57fa12093c79bc5d53b1b317cc7ed
agrass <agrass@agrass-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-09-05 13:14:20 -0300

    cambio facebook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ Bitlag::Application.configure do
 
   APP_ID= '226439280795021'
   APP_SECRET= '2c8b79cab5d6bae38e6e5afe1758e0e2'
-  SITE_URL = 'http://iwuanago.herokuapp.com'
+  SITE_URL = 'http://iwuanago.com'
 end
