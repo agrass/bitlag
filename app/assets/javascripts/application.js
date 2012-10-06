b7a6c4c54834a04d87345e95b842ceb3cf2ea63d
agrass <agrass@agrass-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-10-06 13:15:35 -0300

    cambio style y js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-
 //= require jquery-ui
 
      $(function () {
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,4 @@
+//= require jquery.easing.1.3
+//= require jquery.hoverIntent.minified
+//= require diapo
+//= require filters
