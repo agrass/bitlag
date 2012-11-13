cee612923868350c2e2f2edd0f360bfd0eb7bfb5
agrass <agrass@agrass-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-11-13 16:13:04 -0300

    cambios locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,18 @@ before_filter :set_locale
       if params[:locales]
         I18n.locale = params[:locales]
       else
-      #otra forma de obtener el lenguaje
+        if request.location != nil
+          if request.location.country == "Chile"
+            I18n.locale = :enES
+          end
+
+        end
       end
     rescue Exception=>e
       # handle e
     end 
   end
 
-  private
-  def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-  end
 
   
 end
