c203c5bb4f89630883a9a3674fd3ce8ee6dea9bf
agrass <agrass@agrass-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-11-15 16:04:32 -0300

    archivos raros

--- db/users.rb
+++
@@ -1,9 +0,0 @@
-User.create([
-  { :fb_id => "100003783250569", :access_token => "AAADN8gDlHY0BAJJPb2KZC3 aBN6rLwZADuwcfIZAokyQUo4PVsKjaojxj4o8v4EVcylg8ZADTUOlkDxAu4tnJApLaxDgKkiAW2R4vZB eHUZAAZDZD", :scaned => nil, :created_at => "2012-11-05 21:31:07", :updated_at => "2012-11-05 21:31:07", :api_key => "Rxq72VLF5T--8g32EIg-hA" },
-  { :fb_id => "588326140", :access_token => "AAADN8gDlHY0BAD2rCGmDZBu25WdXgBZChlyJKW3kmB1KkKZAeHKN7fMa6jpGHpbZCXNyUZCZCtkbmt53gpzUr7W8eLOJMfZBAbvC8aCsoyCzgZDZD", :scaned => nil, :created_at => "2012-11-05 21:31:39", :updated_at => "2012-11-05 21:31:39", :api_key => "ujgNbeA6epYe3rq8QCQ2qA" },
-  { :fb_id => "727525843", :access_token => "AAADN8gDlHY0BAGvCHAhZBvAaKBGONIm0zpyfpDiH6fnX7vNHsgBLkZAd4AdXZAmhJxsF4WzSY0KIaZBYTR43fssQBUeclXvkIFIUsph8wwZDZD", :scaned => nil, :created_at => "2012-11-05 21:31:39", :updated_at => "2012-11-05 21:31:39", :api_key => "VZ-MpRplI87nr6d3xx6B8A" },
-  { :fb_id => "545574409", :access_token => "AAADN8gDlHY0BAPeMtztG7GJxZCl2cA2uIQ2HibmzT6jjZAy54615Hh6o4GSfynjbjXZA72gnuqYda4nYmE5KPNWhKcp0Ioh7EAJ8GK84gZDZD", :scaned => nil, :created_at => "2012-11-05 21:31:40", :updated_at => "2012-11-05 21:31:40", :api_key => "_ExIADBLacoBTzdoHeLNbg" },
-  { :fb_id => "100000571355411", :access_token => "AAADN8gDlHY0BAGO5pfu4S37FMgl2povSe38f6guAvEKvc1qezfM4u4s2TJeZAPWeohKYl87J09iv3ff6Xm6cljcrAmx9agZBqDbZBwkMgZDZD" },
-  { :fb_id => "855099434", :access_token =>  "AAADN8gDlHY0BALbNBinJRNWQCts4YCkDkqZCO5j074VxZAuPfA398GSffRgJn0b7YOYZBQJRHn6HZCUvaH7wpZCEEMzZAhlWz8bkW9FxVYZBAZDZD" },
-  { :fb_id => "1301043714", :access_token => "AAADN8gDlHY0BAJWVmgIGlAQ6dac97dBG2In4lWuwCTSDAVL4YpKuXeBm7SvG2c8HJD5ArbW7twvAYC10eNsyHGhjV8wIhprhCpF6mgZDZD", :scaned => nil, :created_at => "2012-11-05 21:31:40", :updated_at => "2012-11-05 21:31:40", :api_key => "Iu5gdsrN3pztGEG95MXTGw" }
-], :without_protection => true )
